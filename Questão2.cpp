9fdc2e3a4fd92ac50135ce30433acd7a21f31ce7
chsantos234 <77702696+chsantos234@users.noreply.github.com>
2021-08-31 22:39:57 -0300

    Update Questão2.cpp

--- Questão2.cpp
+++ Questão2.cpp
@@ -1,119 +1,119 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <stdio.h>
-#include <string.h>
+#include<iostream>
+#include<cstdlib>
+#include <locale.h>
 
 using namespace std;
 
-#define MAXTAM 1000
+const int TMax=10;
 
-typedef int TipoApontador;
-typedef char Tipoletra;
+struct tFila {
+    int Inicio, Fim, Tam = 0;
+    int Elem[TMax];
+};
 
-typedef struct{
-  Tipoletra letra;
-} TipoItem;
-
-typedef struct {
-  TipoItem Item[MAXTAM];
-  TipoApontador Topo;
-} TipoPilha;
-
-void itemcolocado(TipoItem *Letra, char a){
-  Letra -> letra = a;
+void Primeiro(tFila F){
+    if(F.Tam == 0)
+    	cout << "Fila Vazia\n" <<endl;
+    else
+    	cout << "1º elemento da Fila: " <<F.Elem[F.Inicio+1]<<endl;
 }
 
-void FPVazia(TipoPilha *Pilha){
-  Pilha -> Topo = 0;
-}
-
-int Vazia (TipoPilha *Pilha){
-  return (Pilha -> Topo == 0);
-}
-
-void Empilha (TipoItem x, TipoPilha *Pilha){
-  if (Pilha -> Topo == MAXTAM){
-    printf("erro, tamanho maximo da pilha\n");
-  }
-  else{
-    Pilha -> Item[Pilha -> Topo] = x;
-    Pilha -> Topo++;
-  }
-}
-
-TipoItem Desempilha (TipoPilha *Pilha){
-  if (Vazia(Pilha)){
-    cout << "erro, pilha vazia" << endl;
-  }
-  else{
-    Pilha -> Topo--;
-    return Pilha -> Item[Pilha -> Topo];
-  }
-}
-
-int Tamanho(TipoPilha *Pilha){
-  return Pilha ->Topo;
-}
-
-string reverter(string frase, TipoItem it, 
-TipoPilha p){
-  for (int i=0; i<frase.length(); i++){
-    if (strcmp(&frase[i], "") != 0){
-      itemcolocado(&it, frase[i]);
-      Empilha(it, &p);
+void Inserir(tFila &F, int V){
+        if(F.Tam == TMax)
+		cout << "Capacidade Esgotada";
+    else {
+        F.Fim = (F.Fim + 1) % TMax;
+        F.Elem[F.Fim] = V;
+        F.Tam++;
     }
-  }
-  int tamanhoPilha = Tamanho(&p) - 1 ;
-  string result = "";
-  for (int i=0; i <= tamanhoPilha ; i++){
-    char t;
-    t = Desempilha(&p).letra;
-    result += t;
-  }
-  return result;
 }
 
-int main() {
-  string frase;
-  
-  cout << "digite:";
-
-  getline(cin,frase);
-
-  for_each(frase.begin(), frase.end(), [](char & c){
-    c = ::tolower(c);
-  });
-  
-  cout << "\nsua frase é: " << frase << "\n" << endl;
-  TipoPilha p;
-
-  FPVazia(&p);
-
-  TipoItem it;
-
-  frase += " ";
-  string sub;
-  string resultado;
-  int count = 0;
-  int space = 0;
-  for (int i=0; i<frase.length(); i++){
-    if (isspace(frase[i]) && count ==0 ){
-      sub = frase.substr(0, i);
-      resultado += reverter(sub, it, p);
-      resultado += " ";
-      count += i + 1;
-      space += 1;
+void FurarFila(tFila &F, int V){
+    if(F.Tam == TMax)
+		cout << "Capacidade Esgotada\n";
+    else {
+        F.Fim = F.Fim+1;
+        for(int i = F.Fim;i>0;i--){
+          F.Elem[i] = F.Elem[i-1]; 
+        }
+        F.Elem[F.Inicio+1] = V;
+        F.Tam++;
     }
-    else if (isspace(frase[i]) && count != 0){
-      sub = frase.substr(count, i-count);
-      resultado += reverter(sub, it, p);
-      resultado += " ";
-      count = i+1;
-      space -= 1;
-    }
-  }
-  cout << "frase revertida: "<<  resultado << endl;
+}
 
-  return 0;
+void Remover(tFila &F) {
+	if(F.Tam == 0)
+	    cout << "Fila Vazia\n";
+   	else{
+    	F.Inicio = (F.Inicio+1)% TMax;
+    	F.Tam--;
+	}
+}
+
+int mapear_valor (int v){
+	return v % TMax;
+}     
+
+void Visualiza(tFila F){
+    int cont=1;
+    int aux = mapear_valor(F.Inicio+1);
+    if(F.Tam == 0){
+	    cout << "Fila Vazia\n";
+    }else{
+    while (F.Tam > cont-1){
+    	cout <<cont<< "º valor: "<< F.Elem[aux] << endl;
+    	cont++;
+    	F.Inicio++;
+    	aux = mapear_valor(aux+1);
+    }
+	}
+}
+
+int main(){
+	setlocale(LC_ALL,"PORTUGUESE");
+  tFila F;
+
+    F.Inicio = 0;
+    F.Fim = 0;
+    F.Tam = 0;
+    
+    int V, opc, pos;
+    
+    do{
+    	cout << "Escolha uma opção:\n";
+    	cout << "1 - Acessar o primeiro elemento\n";
+    	cout << "2 - Inserir um elemento\n";
+      cout << "3 - Furar Fila\n";
+    	cout << "4 - Remover um elemento\n";
+    	cout << "5 - Visualizar\n";
+    	cout << "6 - Sair\n";
+    	cin >> opc;
+    	system("clear");
+    
+    	switch(opc){
+	        case 1:
+				    Primeiro(F);
+          break;
+          case 2:
+				    cout <<"Insira um inteiro qualquer:\n";
+            cin >> V;
+            Inserir(F, V);
+        	break;
+          case 3:
+            cout <<"Insira um inteiro qualquer:\n";
+            cin >> V;
+            FurarFila(F,V);
+          break;
+          case 4:
+				    Remover(F);
+          break;
+          case 5:
+				    Visualiza(F); 
+          break;
+          case 6:
+            cout<<"Saindo ...";
+          break;
+   		}
+	}while(opc!=6);
+
 }
